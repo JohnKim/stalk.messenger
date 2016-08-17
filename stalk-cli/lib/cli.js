80e9b1143c0d3a14ee600465b13b2c48bba4a3bd
John Kim <yohany@gmail.com>
2016-08-18 00:01:04 +0900

    add data modeling

--- stalk-cli/lib/cli.js
+++ stalk-cli/lib/cli.js
@@ -1,12 +1,31 @@
 var pkg         = require('../package'),
     chalk       = require('chalk'),
-    program     = require('commander'),
-    commands    = require('./commands');
+    program     = require('commander');
 
 function cli(args) {
     program.version(pkg.version);
 
-    // TODO !!!!!
+    program
+      .command('install-session [path]')
+      .description('install session server.')
+      .option("-g, --global", "install global")
+      .action(function(_path, options){
+
+        var installPath = _path ? path.resolve(_path) : process.cwd();
+        // TODO
+        console.log(installPath);
+
+      }).on('--help', function() {
+
+        console.log('  Examples:');
+        console.log();
+        console.log('    $ stalk install-session ');
+        console.log('    $ stalk install-session /abc/def/gh');
+        console.log();
+
+      });
+
+      program.parse(process.argv);
 
 }
 
