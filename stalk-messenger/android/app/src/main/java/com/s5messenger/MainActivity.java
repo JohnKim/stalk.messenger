035c307a208d8ad8ab773b0bac85352780e21419
John Kim <yohany@gmail.com>
2016-09-12 07:21:08 +0900

    for android

--- stalk-messenger/android/app/src/main/java/com/s5messenger/MainActivity.java
+++ stalk-messenger/android/app/src/main/java/com/s5messenger/MainActivity.java
@@ -1,10 +1,15 @@
 package com.s5messenger;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.gcrabtree.rctsocketio.SocketIoPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.imagepicker.ImagePickerPackage;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends ReactActivity {
 
@@ -16,4 +21,15 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "s5messenger";
     }
+
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+        new MainReactPackage(),
+        new VectorIconsPackage(),
+        new ImagePickerPackage(),
+        new SocketIoPackage(),
+        new LinearGradientPackage()
+      );
+    }
+
 }
